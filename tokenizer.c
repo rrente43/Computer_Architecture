04e3c43e7da8099bcc5170aa1ac5220d70cfc0f3
Rubi Renteria <53914397+rrente43@users.noreply.github.com>
2022-02-23 15:27:53 -0700

    Update tokenizer.c
    Updated Functions and test methods in main.

--- tokenizer.c
+++ tokenizer.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+//#include <string.h>
 /* Return true (non-zero) if c is the delimiter character
    previously chosen by the user.
    Zero terminators are not printable (therefore false) */
@@ -8,7 +10,7 @@ bool delim_character(char c, char delim)
 {
   return (delim != ((c >= '0'&& c<='9')||(c>= 'a'&& c<= 'z')||(c>= 'A'&& c<= 'Z')) && c==delim);
   //return (c==delim);
-
+  //return(c == ' ' && c == '\0');
 }
 
 /* Return true (non-zero) if c is a non-delimiter
@@ -16,7 +18,6 @@ bool delim_character(char c, char delim)
    Zero terminators are not printable (therefore false) */
 bool non_delim_character(char c, char delim)
 {
-  //delim = ((c >= '0'&& c<='9')||(c>= 'a'&& c<= 'z')||(c>= 'A'&& c<= 'Z'));
   return !(delim_character(c,delim));
 
 }
@@ -38,7 +39,6 @@ char *word_start(char* str,char delim)
    terminated string*/
 char *end_word(char* str,char delim)
 {
-  str= word_start(str,delim);
   int i = 0;
   while (non_delim_character(str[i],delim)){
     i++;
@@ -50,12 +50,16 @@ char *end_word(char* str,char delim)
 /* Counts the number of words or tokens*/
 int count_tokens(char* str,char delim)
 {
-  int count;
-  for(count=0; str!=end_word(str,delim); count++){
-    str=end_word(str,delim);
-  }
-  return count;
+  int count = 0;
+  int i=0;
 
+  for (int i = 0; str[i]!='\0'; i++) {
+    if(str[i]== ' '){
+      count++;
+    }
+  }
+  return count+1;
+ printf( "Count is:%d\n",count);
 }
 
 /* Returns a freshly allocated new zero-terminated string
@@ -64,11 +68,12 @@ char *copy_str(char *inStr, short len, char delim)
 {
   int i;
   char* copy = (char*)malloc(sizeof(char)*(len+1));
+
   for(i=0; i<len; i++){
     copy[i]=inStr[i];
   } 
-  copy[i]= '\0';
   return copy;
+  
 
 }
 //citation https://www.youtube.com/watch?v=yFboyOwk2oM
@@ -87,17 +92,18 @@ char** tokenize(char* str, char delim)
   int i;
   int numtokens = count_tokens(str,delim);
 
-  char **array = malloc(sizeof(char*)*(numtokens));
+  char **array = (char**)malloc(sizeof(char*)*(numtokens));
   char *start = word_start(str,delim);
   char *end = end_word(str,delim);
 
   for(i=0; i<numtokens; i++){
     array[i]= copy_str(start,end-start,delim);
+    printf("Token : %s\n",copy_str(start,end-start,delim));
     start = word_start(end,delim);
     end = end_word(start,delim);
-
+    str = end;
   }
-  array[i]=0;
+
   return array;
 
 }
@@ -107,22 +113,31 @@ void print_all_tokens(char** tokens)
 {
   int i =0;
   while(tokens[i]){
-    printf(i,tokens[i]);
-    i++;
+    printf("Tokens: %c",**tokens);
+    tokens++;
   }
   return;
 
 }
 
-int main(){
-  char input[]= "Hello, World";
+int main(void){
+  char input[]= "Hello, World";  
   char *pinput =input;
   char deli = ',';
 
   char *x = word_start(pinput,deli);
   printf("The first letter is: %c\n",*x);
 
-  x = end_word(pinput, deli) ;
-  printf ("The delimeter is: %c",* x);
+  char *y = end_word(pinput,deli);
+  printf("The delimeter is: %c\n",*y);
+
+  int count= count_tokens (pinput, deli);
+  printf("Number of Tokens: %d\n" , count);
+
+  char *copy = copy_str(pinput, strlen(pinput),deli);
+  printf("Copied string:  %s\n",copy);
+
+  char tokenizer = tokenize(pinput,deli);
+  
 
 }
